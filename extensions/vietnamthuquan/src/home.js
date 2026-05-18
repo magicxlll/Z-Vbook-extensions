6a906e91e736e2e0f60d7b5558d3dcb539f11314
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-05-18 15:17:15 +0700

    Upload extensions/vietnamthuquan/src/home.js

--- extensions/vietnamthuquan/src/home.js
+++ extensions/vietnamthuquan/src/home.js
@@ -4,15 +4,35 @@ load("config.js");
 
 function execute() {
     return Response.success([
-    {
-        "title": "Mới cập nhật",
-        "input": "http://vietnamthuquan.eu",
-        "script": "gen.js"
-    },
-    {
-        "title": "Hoàn thành",
-        "input": "http://vietnamthuquan.eu/hoan-thanh/",
-        "script": "gen.js"
-    }
-]);
+        {
+            "title": "Tất cả Sách",
+            "input": BASE_URL + "/truyen/default.aspx",
+            "script": "gen.js"
+        },
+        {
+            "title": "Kiếm Hiệp",
+            "input": BASE_URL + "/truyen/theloai.aspx?theloaiid=6",
+            "script": "gen.js"
+        },
+        {
+            "title": "Tiên Hiệp, Tu Chân",
+            "input": BASE_URL + "/truyen/theloai.aspx?theloaiid=32",
+            "script": "gen.js"
+        },
+        {
+            "title": "Tiểu Thuyết",
+            "input": BASE_URL + "/truyen/theloai.aspx?theloaiid=23",
+            "script": "gen.js"
+        },
+        {
+            "title": "Trinh Thám, Hình Sự",
+            "input": BASE_URL + "/truyen/theloai.aspx?theloaiid=12",
+            "script": "gen.js"
+        },
+        {
+            "title": "Khoa Huyễn, Giả Tưởng",
+            "input": BASE_URL + "/truyen/theloai.aspx?theloaiid=31",
+            "script": "gen.js"
+        }
+    ]);
 }
