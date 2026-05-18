12e0f3d90740075f8240b58c058a049dabf1e9f5
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-05-18 14:52:56 +0700

    Upload extensions/vietnamthuquan/src/config.js

--- extensions/vietnamthuquan/src/config.js
+++ extensions/vietnamthuquan/src/config.js
@@ -1,2 +1,10 @@
 var BASE_URL = "http://vietnamthuquan.eu";
 try { if (CONFIG_URL) BASE_URL = CONFIG_URL; } catch (e) {}
+
+function fetchBook(url, options) {
+    if (!options) options = {};
+    if (!options.headers) options.headers = {};
+    options.headers["Cookie"] = "AspxAutoDetectCookieSupport=1";
+    options.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+    return fetch(url, options);
+}
