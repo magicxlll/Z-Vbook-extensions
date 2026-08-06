880a47443aba94c59dcfc5bbdc47dded01d676a8
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:40:16 +0700

    Upload extensions/vireal/src/toc.js

--- extensions/vireal/src/toc.js
+++ extensions/vireal/src/toc.js
@@ -1,34 +1,40 @@
 load("config.js");
 
 function execute(url) {
-    url = url.replace(/^(?:https?://)?(?:[^@
-]+@)?(?:www.)?([^:/
-?]+)/img, BASE_URL);
+    url = url.replace(/^https?:\/\/[^\/]+/, BASE_URL);
 
     var res = fetchBook(url);
     if (!res.ok) return Response.error("Cannot load: " + res.status);
 
     var html = res.text();
 
-    var mSlug = html.match(/canonical"s+href="https://vireal.vn//story/([^"]+)"/) || html.match(/\"slug\":\"([^\"]+)\"/);
-    var storySlug = mSlug ? mSlug[1] : url.split("/story/")[1].split("/")[0].split("?")[0];
+    var parts = url.split("/story/");
+    var storySlug = parts.length > 1 ? parts[1].split("/")[0].split("?")[0] : "";
+    var mStorySlug = html.match(/canonical"\s+href="https:\/\/vireal\.vn\/\/story\/([^"]+)"/);
+    if (mStorySlug) storySlug = mStorySlug[1];
 
     var chapters = [];
     var seen = {};
 
-    var chapRegex = /\"id\":\"(\d+)\",\"name\":\"([^\"]+)\",\"slug\":\"([^\"]+)\"/g;
-    var match;
-    while ((match = chapRegex.exec(html)) !== null) {
-        var chapName = match[2];
-        var chapSlug = match[3];
-        var chapUrl = BASE_URL + "/story/" + storySlug + "/" + chapSlug;
-        if (!seen[chapUrl]) {
-            seen[chapUrl] = true;
-            chapters.push({
-                name: chapName,
-                url: chapUrl,
-                host: BASE_URL
-            });
+    var chapBlocks = html.split(/\{\"author\":null,\"id\":\"/);
+    for (var i = 1; i < chapBlocks.length; i++) {
+        var block = chapBlocks[i];
+        var mName = block.match(/\"name\":\"([^\"]+)\"/);
+        var mChapSlug = block.match(/\"slug\":\"([^\"]+)\"/);
+
+        if (mName && mChapSlug) {
+            var cName = mName[1];
+            var cSlug = mChapSlug[1];
+            var cUrl = BASE_URL + "/story/" + storySlug + "/" + cSlug;
+
+            if (!seen[cUrl]) {
+                seen[cUrl] = true;
+                chapters.push({
+                    name: cName,
+                    url: cUrl,
+                    host: BASE_URL
+                });
+            }
         }
     }
 
