5854b4a905c02a0c4d0e11c6007f14bed4855cc9
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:11:03 +0700

    Upload extensions/storya/src/config.js

---
+++ extensions/storya/src/config.js
@@ -0,0 +1,10 @@
+var BASE_URL = "https://storya.click";
+try { if (CONFIG_URL) BASE_URL = CONFIG_URL; } catch (e) {}
+
+function fetchBook(url, options) {
+    options = options || {};
+    options.headers = options.headers || {};
+    options.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36";
+    options.headers["Referer"] = BASE_URL + "/";
+    return fetch(url, options);
+}
