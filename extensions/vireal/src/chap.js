cbe13216f2ec7592332a3f19b61c9b70a01b02ff
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:25:15 +0700

    Upload extensions/vireal/src/chap.js

--- extensions/vireal/src/chap.js
+++ extensions/vireal/src/chap.js
@@ -8,13 +8,27 @@ function execute(url) {
     var res = fetchBook(url);
     if (!res.ok) return Response.error("Cannot load: " + res.status);
 
-    var doc = res.html();
+    var html = res.text();
+    var cleanHtml = html.replace(/\u003c/g, "<").replace(/\u003e/g, ">").replace(/\"/g, '"');
+
+    cleanHtml = cleanHtml.replace(/<[a-z0-9]+\s+style="text-indent:-9144px;[^"]*">[^<]*<\/[a-z0-9]+>/gi, "");
+    cleanHtml = cleanHtml.replace(/<style>[^<]*<\/style>/gi, "");
+
+    var doc = Html.parse(cleanHtml);
 
     doc.select("script, style, ins, iframe, noscript").remove();
-    doc.select(".ads, .banner, [class*='ads']").remove();
+    doc.select("[style*='text-indent'], [style*='font-size: 0px'], [style*='font-size:0px']").remove();
 
-    var contentEl = doc.select(".chapter-content, .content, article, main").first();
-    if (!contentEl) return Response.error("Không tìm thấy nội dung chương");
+    var contentEl = doc.select(".chapter-content, #reading-content, article, main, div[class*='Content']").first();
+    if (!contentEl) {
+        var paras = doc.select("p");
+        if (paras.size() > 0) {
+            var sb = "";
+            paras.forEach(function(p) { sb += "<p>" + p.html() + "</p>"; });
+            return Response.success(sb);
+        }
+        return Response.error("Không tìm thấy nội dung chương");
+    }
 
     var content = contentEl.html() + "";
     content = content.replace(/&nbsp;/g, " ");
