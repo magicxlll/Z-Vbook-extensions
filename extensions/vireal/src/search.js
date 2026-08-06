04cfa52baa45eaee5d4750ef49430a1f0f879eea
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:40:14 +0700

    Upload extensions/vireal/src/search.js

--- extensions/vireal/src/search.js
+++ extensions/vireal/src/search.js
@@ -1,7 +1,6 @@
 load("config.js");
 
 function execute(key, page) {
-    page = page || "1";
     var fetchUrl = BASE_URL + "/search?q=" + encodeURIComponent(key);
 
     var res = fetchBook(fetchUrl);
@@ -11,47 +10,29 @@ function execute(key, page) {
     var list = [];
     var seen = {};
 
-    var storyRegex = /\"slug\":\"([^\"]+)\"[\s\S]*?\"name\":\"([^\"]+)\"[\s\S]*?\"thumbnail\":\"([^\"]+)\"/g;
-    var m;
-    while ((m = storyRegex.exec(html)) !== null) {
-        var slug = m[1];
-        var name = m[2];
-        var cover = m[3];
-        var link = BASE_URL + "/story/" + slug;
-        if (slug && name && !seen[link]) {
-            seen[link] = true;
-            list.push({
-                name: name,
-                link: link,
-                cover: cover,
-                host: BASE_URL
-            });
+    var blocks = html.split(/\{\"id\":\"/);
+    for (var i = 1; i < blocks.length; i++) {
+        var block = blocks[i];
+        var mSlug = block.match(/\"slug\":\"([^\"]+)\"/);
+        var mName = block.match(/\"name\":\"([^\"]+)\"/);
+        var mThumb = block.match(/\"thumbnail\":\"([^\"]+)\"/);
+
+        if (mSlug && mName) {
+            var slug = mSlug[1];
+            var name = mName[1];
+            var cover = mThumb ? mThumb[1] : "";
+            var link = BASE_URL + "/story/" + slug;
+
+            if (!seen[link]) {
+                seen[link] = true;
+                list.push({
+                    name: name,
+                    link: link,
+                    cover: cover,
+                    host: BASE_URL
+                });
+            }
         }
-    }
-
-    if (list.length === 0) {
-        var doc = res.html();
-        doc.select("a[href^='/story/']").forEach(function (el) {
-            var href = (el.attr("href") || "") + "";
-            if (!href || href === "/story/") return;
-            var link = href.indexOf("http") === 0 ? href : BASE_URL + href;
-            if (seen[link]) return;
-            seen[link] = true;
-
-            var name = (el.select("h3, h2").text() || el.text() || "").trim();
-            var img = el.select("img").first();
-            var cover = "";
-            if (img) cover = (img.attr("src") || img.attr("data-src") || "") + "";
-            if (!name && img) name = (img.attr("alt") || "").trim();
-            if (!name) return;
-
-            list.push({
-                name: name,
-                link: link,
-                cover: cover,
-                host: BASE_URL
-            });
-        });
     }
 
     return Response.success(list);
