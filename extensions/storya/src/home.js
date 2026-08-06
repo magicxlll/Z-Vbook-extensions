59051a62850b1060c28942dfedd8b1293947aa90
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:11:08 +0700

    Upload extensions/storya/src/home.js

---
+++ extensions/storya/src/home.js
@@ -0,0 +1,9 @@
+load("config.js");
+
+function execute() {
+    return Response.success([
+        { title: "Truyện Mới", input: BASE_URL + "/danh-sach/truyen-moi", script: "gen.js" },
+        { title: "Truyện Hot", input: BASE_URL + "/danh-sach/truyen-hot", script: "gen.js" },
+        { title: "Truyện Full", input: BASE_URL + "/danh-sach/truyen-full", script: "gen.js" }
+    ]);
+}
