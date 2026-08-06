09128e6b74bf9ce21e9c1a95fe5ceb301b181f62
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 18:45:39 +0700

    Upload extensions/conduongbachu/src/page.js

--- extensions/conduongbachu/src/page.js
+++ extensions/conduongbachu/src/page.js
@@ -4,5 +4,20 @@ function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
     if (url.slice(-1) === "/") url = url.slice(0, -1);
 
+    if (url.indexOf("/ngoai-truyen") > -1) {
+        var res = fetchBook(url + "/");
+        if (res.ok) {
+            var doc = res.html();
+            var pages = [url + "/"];
+            doc.select(".nav-pagination a.page-number").forEach(function (el) {
+                var href = (el.attr("href") || "") + "";
+                if (href && href.indexOf("http") === 0 && pages.indexOf(href) === -1) {
+                    pages.push(href);
+                }
+            });
+            return Response.success(pages);
+        }
+    }
+
     return Response.success([url]);
 }
