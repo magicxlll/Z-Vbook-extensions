8fb10c811236d1d05083f72af17805537eeefc9d
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 18:45:37 +0700

    Upload extensions/conduongbachu/src/home.js

--- extensions/conduongbachu/src/home.js
+++ extensions/conduongbachu/src/home.js
@@ -3,28 +3,18 @@ load("config.js");
 function execute() {
     return Response.success([
         {
-            title: "Con Đường Bá Chủ",
+            title: "Tất Cả Bộ Truyện",
+            input: BASE_URL + "/all-stories/",
+            script: "gen.js"
+        },
+        {
+            title: "Truyện Chính (3752+ Chương)",
             input: BASE_URL + "/chuong-3752-ket-thuc-se-la-noi-bat-dau/",
             script: "gen.js"
         },
         {
-            title: "Tất cả Chapter",
-            input: BASE_URL + "/chapter-truyen/",
-            script: "gen.js"
-        },
-        {
-            title: "Bất Hủ Thần Chiến",
-            input: BASE_URL + "/ngoai-truyen/",
-            script: "gen.js"
-        },
-        {
-            title: "Vạn Đạo Thần Chủ",
-            input: BASE_URL + "/ngoai-truyen-van-dao-than-chu/",
-            script: "gen.js"
-        },
-        {
-            title: "Chúa Tể Chi Lộ",
-            input: BASE_URL + "/ngoai-truyen-chua-te-chi-lo/",
+            title: "Ngoại Truyện",
+            input: BASE_URL + "/all-ngoai-truyen/",
             script: "gen.js"
         }
     ]);
