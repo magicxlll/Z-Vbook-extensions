9a6ed76838271fac4b0d4bbe40b3a238a76261b6
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:25:22 +0700

    Upload extensions/vireal/src/home.js

--- extensions/vireal/src/home.js
+++ extensions/vireal/src/home.js
@@ -2,7 +2,7 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "Top Thịnh Hành", input: BASE_URL + "/real/trending", script: "gen.js" },
+        { title: "Top Thịnh Hành", input: BASE_URL + "/", script: "gen.js" },
         { title: "Mới Cập Nhật", input: BASE_URL + "/real/recent", script: "gen.js" }
     ]);
 }
