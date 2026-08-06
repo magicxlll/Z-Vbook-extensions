60bd310bfb0250f423448bef9163fc3322d4715e
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-06 23:12:08 +0700

    Upload extensions/vireal/src/gen.js

---
+++ extensions/vireal/src/gen.js
@@ -0,0 +1,43 @@
+load("config.js");
+
+function execute(url, page) {
+    page = page || "1";
+    var fetchUrl = url;
+    if (page !== "1") {
+        fetchUrl += "?page=" + page;
+    }
+
+    var res = fetchBook(fetchUrl);
+    if (!res.ok) return Response.error("Cannot load: " + res.status);
+
+    var doc = res.html();
+    var list = [];
+
+    doc.select("a[href^='/story/']").forEach(function (el) {
+        var href = (el.attr("href") || "") + "";
+        if (!href || href === "/story/") return;
+
+        var name = (el.select("h3, h2").text() || el.text() || "").trim();
+        var img = el.select("img").first();
+        var cover = "";
+        if (img) cover = (img.attr("src") || img.attr("data-src") || "") + "";
+
+        if (!name && img) name = (img.attr("alt") || "").trim();
+        if (!name) return;
+
+        var link = href.indexOf("http") === 0 ? href : BASE_URL + href;
+
+        list.push({
+            name: name,
+            link: link,
+            cover: cover,
+            host: BASE_URL
+        });
+    });
+
+    var nextPage = null;
+    var nextBtn = doc.select("a[href*='page=']").last();
+    if (nextBtn) nextPage = String(parseInt(page) + 1);
+
+    return Response.success(list, nextPage);
+}
