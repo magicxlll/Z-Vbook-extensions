98edc835f5e2c19c198257757a9911a57ea029dd
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 13:47:53 +0700

    Upload extensions/xtruyen/src/toc.js

--- extensions/xtruyen/src/toc.js
+++ extensions/xtruyen/src/toc.js
@@ -1,1 +1,37 @@
-function execute(url) { return Response.success([]); }
+load("config.js");
+
+function execute(url) {
+    var mangaId = "";
+    var doc = fetch(url).html();
+    var mangaIdElement = doc.select("#manga-chapters-holder").first();
+    
+    if (mangaIdElement) {
+        mangaId = mangaIdElement.attr("data-id");
+    }
+
+    if (mangaId) {
+        var res = fetch("https://xtruyen.vn/wp-admin/admin-ajax.php", {
+            method: "POST",
+            body: "action=manga_get_chapters&manga=" + mangaId
+        });
+        if (res.ok) {
+            doc = res.html();
+        }
+    }
+
+    var list = [];
+    var els = doc.select("li.wp-manga-chapter a");
+    if (els.size() === 0) els = doc.select(".chapter-title");
+
+    // Madara chapters are usually loaded from newest to oldest, so we reverse it
+    for (var i = els.size() - 1; i >= 0; i--) {
+        var e = els.get(i);
+        list.push({
+            name: e.text().trim(),
+            url: e.attr("href"),
+            host: "https://xtruyen.vn"
+        });
+    }
+
+    return Response.success(list);
+}
