1a695d2bc1bdb491560e3f5756141a9c70099dcd
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 09:54:17 +0700

    Upload extensions/banlong/src/newchap.js

--- extensions/banlong/src/newchap.js
+++ extensions/banlong/src/newchap.js
@@ -2,21 +2,21 @@ load("config.js");
 
 function execute(url, page) {
     if (!page) page = '1';
-    let response = fetch(url + "?page=" + page);
+    var response = fetch(url + "?page=" + page);
     if (response.ok) {
-        let doc = response.html();
+        var doc = response.html();
 
-        let nextPage = /page=(\d+)/.exec(doc.select(".next-page").first().attr("href"));
+        var nextPage = /page=(\d+)/.exec(doc.select(".next-page").first().attr("href"));
         if (nextPage) nextPage = nextPage[1];
         else nextPage = "";
 
-        let books = [];
-        doc.select(".list-story__item li").forEach(e => {
+        var books = [];
+        doc.select(".list-story__item li").forEach(function(e) {
             books.push({
                 name: e.select("p.line-clamp-1").text(),
                 link: e.select("a").attr("href").replace(/chuong-\d+/g, ""),
                 description: e.select("a.line-clamp-1").text(),
-                host: BASE_URL,
+                host: BASE_URL
             });
         });
 
