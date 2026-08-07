2928441ae8b1107cd1090571f2b48ad2411359d8
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 13:47:47 +0700

    Upload extensions/xtruyen/src/detail.js

--- extensions/xtruyen/src/detail.js
+++ extensions/xtruyen/src/detail.js
@@ -1,1 +1,22 @@
-function execute(url) { return Response.success({name: 'Test', cover: '', host: 'https://xtruyen.vn'}); }
+load("config.js");
+
+function execute(url) {
+    var response = fetch(url);
+    if (response.ok) {
+        var doc = response.html();
+        var name = doc.select(".post-title h1").text();
+        var author = doc.select(".author-content a").text();
+        var cover = doc.select(".summary_image img").attr("src") || doc.select(".summary_image img").attr("data-src") || "https://xtruyen.vn/wp-content/themes/madara/images/no-image.png";
+        var desc = doc.select(".summary-content.vote-details").text() + " - " + doc.select(".genres-content").text();
+        
+        return Response.success({
+            name: name.trim(),
+            cover: cover,
+            author: author,
+            description: desc,
+            detail: "Tác giả: " + author,
+            host: "https://xtruyen.vn"
+        });
+    }
+    return Response.error("Lỗi khi tải trang");
+}
