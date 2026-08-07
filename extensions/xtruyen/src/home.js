f5ba433dd3aad1b3989035867040ae2368678c29
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 13:47:51 +0700

    Upload extensions/xtruyen/src/home.js

--- extensions/xtruyen/src/home.js
+++ extensions/xtruyen/src/home.js
@@ -1,1 +1,10 @@
-function execute() { return Response.success([]); }
+load("config.js");
+
+function execute() {
+    return Response.success([
+        { title: "Mới cập nhật", input: "https://xtruyen.vn/truyen/?m_orderby=latest", script: "gen.js" },
+        { title: "Truyện mới", input: "https://xtruyen.vn/truyen/?m_orderby=new-manga", script: "gen.js" },
+        { title: "Truyện Hot", input: "https://xtruyen.vn/truyen/?m_orderby=views", script: "gen.js" },
+        { title: "Truyện Full", input: "https://xtruyen.vn/theloai/truyen-full/", script: "gen.js" }
+    ]);
+}
