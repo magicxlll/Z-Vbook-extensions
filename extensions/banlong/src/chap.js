f0a75988deed33781664876e4d99852df33635dc
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 15:33:45 +0700

    Upload extensions/banlong/src/chap.js

--- extensions/banlong/src/chap.js
+++ extensions/banlong/src/chap.js
@@ -4,8 +4,11 @@ function execute(url) {
     var response = fetchBook(url);
     if (response.ok) {
         var doc = response.html();
+        if (doc.select("#chapter-content .content-lock").text().length > 10) {
+            return Response.error("Bạn cần trả phí chương này để có thể đọc.");
+        }
         doc.select("script, style, iframe, ins, .ads").remove();
-        var contentEl = doc.select(".chapter-content, #chapter-content, .box-chap, .s-content").first();
+        var contentEl = doc.select("#chapter-content .s-content").first();
         if (contentEl) {
             return Response.success(contentEl.html());
         }
