a56441d5e28bbcf691edbbe72d2e304fe3f9c539
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 08:30:32 +0700

    Upload extensions/storya/src/home.js

--- extensions/storya/src/home.js
+++ extensions/storya/src/home.js
@@ -2,6 +2,8 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "Tất cả truyện", input: "all", script: "homecontent.js" }
+        { title: "Truyện Mới", input: "/stories?sort=newest", script: "homecontent.js" },
+        { title: "Truyện Hot", input: "/stories?sort=popular", script: "homecontent.js" },
+        { title: "Hoàn Thành", input: "/stories?status=COMPLETED", script: "homecontent.js" }
     ]);
 }
