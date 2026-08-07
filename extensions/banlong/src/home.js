c44dcced5c4d64d2684bab7a71dbc859522fd082
magicxlll <116327040+magicxlll@users.noreply.github.com>
2026-08-07 08:29:00 +0700

    Upload extensions/banlong/src/home.js

--- extensions/banlong/src/home.js
+++ extensions/banlong/src/home.js
@@ -2,12 +2,8 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        {title: "Top linh phiếu", input: BASE_URL + "/top-linh-phieu-tuan", script: "source.js"},
-        {title: "Mới nhất", input: BASE_URL + "/truyen-moi-nhat", script: "source.js"},
-        {title: "Mới cập nhật", input: BASE_URL + "/chuong-moi-cap-nhat", script: "newchap.js"},
-        {title: "Top yêu thích", input: BASE_URL + "/truyen-yeu-thich", script: "source.js"},
-        {title: "Truyện hot", input: BASE_URL + "/truyen-hot", script: "source.js"},
-        {title: "Thịnh hành tuần", input: BASE_URL + "/truyen-thinh-hanh-trong-tuan", script: "newchap.js"},
-        {title: "Hoàn thành", input: BASE_URL + "/truyen-hoan-thanh", script: "source.js"}
+        { title: "Truyện Hot", input: BASE_URL + "/truyen-hot", script: "search.js" },
+        { title: "Truyện Mới", input: BASE_URL + "/truyen-moi", script: "search.js" },
+        { title: "Truyện Full", input: BASE_URL + "/truyen-full", script: "search.js" }
     ]);
 }
